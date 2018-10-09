9ba4fca50c0bf775942e5258e91d5b171fbcefad
apinodominguez <apinodominguez@nautilus3.danielcastelao.org>
2018-10-09 09:17:06 +0200

    Commit Boletin1_1

---
+++ Boletin1_1/src/boletin1_1/Boletin1_1.java
@@ -0,0 +1,14 @@
+
+package boletin1_1;
+
+public class Boletin1_1 {
+
+    public static void main(String[] args) {
+    int base = 4, altura = 3, area;
+    //area = base * altura / 2;
+    //System.out.println("area = " + area);
+    //Tamen e posible
+    System.out.println("area = " +base*altura);
+    }
+    
+}
