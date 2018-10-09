9a0608474ef396affbfdeca74816e4d03d281fb4
apinodominguez <apinodominguez@nautilus3.danielcastelao.org>
2018-10-09 09:21:56 +0200

    Boletin1_5 terminado

---
+++ Boletin1_5/src/boletin1_5/Boletin1_5.java
@@ -0,0 +1,16 @@
+
+package boletin1_5;
+import java.util.Scanner;
+
+public class Boletin1_5 {
+
+    public static void main(String[] args) {
+        Scanner ler = new Scanner(System.in);
+        float factorConversion = 1852;
+        System.out.println("introduzca las millas marinas : ");
+        float millasMarinas = ler.nextFloat();
+        System.out.println("seran " + (millasMarinas*factorConversion) + " metros");
+        
+    }
+    
+}
